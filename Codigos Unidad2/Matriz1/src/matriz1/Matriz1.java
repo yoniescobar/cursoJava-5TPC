841e46376312f3773300b054a27202f1f0dbb76d
Yoni Escobar <yoniedilzar@gmail.com>
2021-04-27 08:41:35 -0600

    Clase 27-04-2021

--- Codigos Unidad2/Matriz1/src/matriz1/Matriz1.java
+++ Codigos Unidad2/Matriz1/src/matriz1/Matriz1.java
@@ -4,13 +4,17 @@ package matriz1;
 public class Matriz1 {
 
     public static void main(String[] args) {
-       int matriz[][] = {{1,2,3},{4,5,6},{7,8,9}};
+       int matriz[][] = {{1,2,3},
+                         {4,5,6},
+                         {7,8,9},
+                         {10,11,12}
+                         };
        
         //System.out.println("M[2][3]="+matriz[2][3]); Mostrar un elemento
         
-        for(int i=0;i<3;i++){ //recorre las filas
+        for(int i=0;i<4;i++){ //recorre las filas
             for(int j=0;j<3;j++){ //recorre las columna
-                System.out.print(matriz[i][j] + " ");
+                System.out.print(matriz[i][j] + "\t ");
             }
             System.out.println("");
         }
